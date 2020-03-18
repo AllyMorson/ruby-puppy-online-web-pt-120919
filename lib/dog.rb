a18d2392e8ec13f4127264c708b2153c345bd184
Ally Morson <ally.morson@gmail.com>
2020-03-18 14:48:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+
+  @@all = []
+  
+  def initialize(name)
+    @name =  name
+  #  @@all << self 
+    save 
+  end
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.print_all 
+  #iterates over all of the individual dogs stored in the @@all array and puts out their name to the terminal
+      puts @@all.map {|dog| dog.name} 
+  end 
+  
+   def self.clear_all
+    @@all.clear 
+  end
+  
+  def save
+    @@all << self
+  end 
+  
+end 
